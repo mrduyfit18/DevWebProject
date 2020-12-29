07734045deb913014abcd86d915dcb10334a6de5
mrduyfit18 <18120171@student.hcmus.edu.vn>
2020-12-30 02:56:11 +0700

    Cập nhật  giao diện web

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,8 +2,7 @@ const productsModel = require('../models/productsModel');
 
 exports.index = async (req, res, next) => {
     // Get products from model
-    const Desktops = await productsModel.getProductByTypeAndNumber('Desktops',4);
-    const Hybrids = await productsModel.getProductByTypeAndNumber('Hybrids',3);
-    const Tablets = await productsModel.getProductByTypeAndNumber('Tablets',3);
-    res.render('index', { Desktops, Hybrids, Tablets, onHome: 'active'});
+    const Desktops = await productsModel.getProductByTypeAndNumber('Desktops',3);
+    const Laptops = await productsModel.getProductByTypeAndNumber('Laptops',4);
+    res.render('index', { Desktops, Laptops, onHome: 'active'});
 };
